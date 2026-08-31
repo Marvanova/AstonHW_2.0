fd48f413216d903b4a28d001cdb88727c7117ce5
Asia <marvanovaasia@gmail.com>
2026-08-31 16:03:21 +0500

    add new Task_2

--- H.W_2.6/src/main/java/org/example/Task_2/Main.java
+++ H.W_2.6/src/main/java/org/example/Task_2/Main.java
@@ -1,4 +1,17 @@
 package org.example.Task_2;
 
 public class Main {
+    public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add("Ахмадуллина", "4-75-80");
+        phoneBook.add("Ахтариева", "4-55-63");
+
+        phoneBook.add("Кудрявцева", "4-46-16");
+        phoneBook.add("Ахмадуллина", "3-10-72");
+
+        phoneBook.get("Ахмадуллина");
+        phoneBook.get("Ахтариева");
+    }
 }
